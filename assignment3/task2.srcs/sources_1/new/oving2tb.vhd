db96526b0e80f87e1644f0b893f0a9b3396333a9
lauritsriple <laurits.triple@gmail.com>
2016-09-05 19:32:15 +0200

    Assignment 2 is done!

---
+++ assignment3/task2.srcs/sources_1/new/oving2tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/05/2016 06:32:01 PM
+-- Design Name: 
+-- Module Name: oving2tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity oving2tb is
+--  Port ( );
+end oving2tb;
+
+architecture Behavioral of oving2tb is
+
+begin
+
+
+end Behavioral;
