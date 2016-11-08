1e060633ce76525aa19901d2003c2d93455cc645
Sindre Hansen <sindrehansen92@gmail.com>
2016-11-08 14:09:36 +0100

    Update gitignore and remove compiler output

--- assignment4/assignment4.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.2\\bin
-call %xv_path%/xelab  -wto 3a5fb9ab55694f4aae686dadcec8a0bf -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot thing_with_latch_behav xil_defaultlib.thing_with_latch -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
